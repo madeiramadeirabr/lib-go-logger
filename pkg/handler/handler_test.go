5905ac947511944a1bf5eaf7d5e9105c04ccf534
Lucas Block <lucas.blockv@gmail.com>
2023-05-26 16:55:11 -0300

    chore: adiciona teste para o handler

--- pkg/handler/handler_test.go
+++ pkg/handler/handler_test.go
@@ -1,26 +1,31 @@
 package handler_test
 
 import (
-	"github.com/stretchr/testify/assert"
+	"fmt"
 	h "lib-go-logger/pkg/handler"
 	l "lib-go-logger/pkg/log_level"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestHandler(t *testing.T) {
 	t.Run("Stdout", func(t *testing.T) {
-		t.Run("Should return false when log level is not handling", func(t *testing.T) {
+		t.Run("Should return true when log level is handling", func(t *testing.T) {
 
-			handler := h.New(l.LogLevelDebug)
-			response := handler.Write("Mensagem para o stdout")
-			assert.True(t, response)
-			assert.False(t, false)
+			logLevels := [6]l.LogLevelEnum{l.LogLevelTrace, l.LogLevelDebug, l.LogLevelInfo, l.LogLevelWarning, l.LogLevelError, l.LogLevelEmergency}
+
+			for i, logLevel := range logLevels {
+				t.Run(fmt.Sprintf("Should return correct boolean when log level is %s", logLevel), func(t *testing.T) {
+					handler := h.New(logLevel)
+					for j, messageLevel := range logLevels {
+						assert.Equal(t, j >= i, handler.IsHandling(messageLevel))
+					}
+				})
+
+			}
 		})
 
-		t.Run("Should return true when log level is not handling", func(t *testing.T) {
-
-			assert.True(t, true)
-		})
 		t.Run("Should return true when write to stdout", func(t *testing.T) {
 
 			handler := h.New(l.LogLevelDebug)
